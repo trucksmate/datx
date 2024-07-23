9632a9b7b84a9be8584da7fa249838eaf7f68e71
trucksmate <trucksmate@gmail.com>
2024-07-23 15:14:20 +0500

    Create func2.js

---
+++ func2.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const passwordToCheck = document.getElementById("passwordToCheck").value;
+  const csvUrl = "https://raw.githubusercontent.com/Leoneyy/datx/main/pass.csv";
+
+  fetch(csvUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error');
+      }
+      return response.text();
+    })
+    .then(data => {
+      const isPasswordPresent = data.split('\n').some(line => line.includes(passwordToCheck));
+      if (isPasswordPresent) {
+        loadPopupContent();
+      } else {
+        showError();
+      }
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+      showError();
+    });
+
+  function loadPopupContent() {
+    if (typeof loadContent === 'function') {
+      loadContent();
+    } else {
+      console.error('loadContent function is not defined in popup.js');
+    }
+  }
+
+  function showError() {
+    document.getElementById("root").innerHTML = `
+      <p><b>Account Suspended</b><br/><br/>Renew:<br/>03200485873</p>
+    `;
+  }
+});
